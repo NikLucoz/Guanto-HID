a5a1c5385090b78f7d142dd382ed25cb44f68361
Niccolò Lucozzi <lucozzi.lavoro@gmail.com>
2023-06-01 16:25:23 +0200

    driver.py added

---
+++ Python Code/driver.py
@@ -0,0 +1,29 @@
+import serial
+import keyboard
+import serial
+
+# Imposta la porta seriale e il baud rate
+porta_seriale = 'COM3'  # Sostituisci con la tua porta seriale corretta
+baud_rate = 9600
+
+# Apre la connessione seriale
+ser = serial.Serial(porta_seriale, baud_rate)
+if not ser.isOpen():
+    ser.open()
+    ser.nonblocking()
+print('com3 is open', ser.isOpen())
+while True:
+    # Legge una linea di dati dalla porta seriale
+    linea_dati = ser.readline().decode().strip()
+
+    match linea_dati:
+        case "PAGEUP":
+            keyboard.write('up', delay=0)
+        case "PAGEDOWN":
+            keyboard.write('down',delay=0)
+        case "STOP":
+            # Chiude la connessione seriale
+            ser.close()
+            exit()
+
+
